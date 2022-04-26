b4dc8335b3264c6ce6faa39cb2a2c46d569ef30d
Sauyon Lee <git@sjle.co>
2022-04-26 16:20:41 -0700

    add poetry and extract more info

--- extract.py
+++ extract.py
@@ -9,7 +9,8 @@ import UnityPy
 assert __name__ == "__main__"
 
 parser = argparse.ArgumentParser(description='Extracts card art from MTGA.')
-parser.add_argument("card_re", help="Regex to match card names")
+parser.add_argument("card_re", nargs="?", help="Regex to match card names")
+parser.add_argument("--db", default="card_data.json", help="Path to card database to create or use")
 parser.add_argument("--set", help="Three letter set code to filter for")
 parser.add_argument("--output", default="out", help="Output directory")
 parser.add_argument("--mtga", default="C:\Program Files\Wizards of the Coast\MTGA", help="Path to MTGA install directory")
@@ -19,7 +20,12 @@ args = parser.parse_args()
 
 card_re = args.card_re
 set = args.set
-print("Searching for card with regex: " + card_re)
+if card_re is None:
+    print("Refreshing card database...")
+    if os.path.exists(args.db):
+        os.remove(args.db)
+else:    
+    print(f"Searching for card with regex: '{card_re}'")
 
 mtga_folder = args.mtga
 # UNITY_VERSION = "2020.3.13f1" # doesn't seem to be needed
@@ -28,8 +34,8 @@ mtg_data_folder = os.path.join(mtga_folder, "MTGA_Data", "Downloads")
 art_folder = os.path.join(mtg_data_folder, "AssetBundle")
 data_folder = os.path.join(mtg_data_folder, "Data")
 
-if os.path.exists("card_data.pickle"):
-    card_list = json.load(open("card_data.json", "rb"))
+if os.path.exists(args.db):
+    card_list = json.load(open(args.db, "r", encoding="utf-8"))
 else:
     try:
         card_data_filename = glob.glob(os.path.join(data_folder, "Data_cards_*.mtga"))[0]
@@ -61,15 +67,42 @@ else:
     card_list = []
     for card_ in cards:
         card = {
+            "grpid": card_["grpid"],
             "name": loc_dict[card_["titleId"]],
             "artId": card_["artId"],
             "set": card_["set"],
-            # can add more info to this dict at some point...
+            "flavor_text": loc_dict[card_["flavorId"]],
+            "set": card_["set"],
+            "types": card_["types"] if "types" in card_ else [],
+            "subtypes": card_["subtypes"] if "subtypes" in card_ else [],
+            "typeText": loc_dict[card_["cardTypeTextId"]],
+            "subtypeText": loc_dict[card_["subtypeTextId"]] if "subtypeTextId" in card_ else None,
+            "frameColors": card_["frameColors"] if "frameColors" in card_ else [],
+            "colorIdentity": card_["colorIdentity"] if "colorIdentity" in card_ else [],
         }
+
+        for attr in ["power", "toughness", "cmc", "colors", "castingcost", "subtypes", "rarity", "collectorNumber", "collectorMax"]:
+            if attr in card_:
+                card[attr] = card_[attr]
+
+        if "artistCredit" in card_:
+            card["artist"] = card_["artistCredit"]
+
+        if "abilities" in card_:
+            card["abilities"] = [
+                {"id": ability["Id"], "text": loc_dict[ability["TextId"]]} for ability in card_["abilities"]
+            ]
+
+        if "castingcost" in card_:
+            cost = card_["castingcost"]
+            card["castingcost"] = cost.replace("o", "")
 
         card_list.append(card)
 
-    json.dump(card_list, open("card_data.json", "w"))
+    json.dump(card_list, open(args.db, "w", encoding="utf-8"), indent=2, ensure_ascii=False)
+
+if card_re is None:
+    exit(0)
 
 # output art from card data
 for card in card_list:
